8fafc88500bbc0927cbbb5a534d00688d28c687d
Alex Dickey <alexander.c.dickey@gmail.com>
2023-12-02 22:05:32 -0800

    style changes

--- INRIX-hack23/src/filterButtons/priceButton.jsx
+++ INRIX-hack23/src/filterButtons/priceButton.jsx
@@ -3,16 +3,12 @@ import './priceButton.css'
 
 const priceButton = ({minPrice, setMinPrice, maxPrice, setMaxPrice}) => {
   const [showDropdown, setShowDropdown] = useState(false);
-  // const [minPrice, setMinPrice] = useState(0);
-  // const [maxPrice, setMaxPrice] = useState(0);
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
   };
 
   const handleSubmit = () => {
-    console.log("minPrice: "+minPrice)
-    console.log("maxPrice: "+maxPrice)
     setShowDropdown(!showDropdown);
   }
 
@@ -23,14 +19,12 @@ const priceButton = ({minPrice, setMinPrice, maxPrice, setMaxPrice}) => {
         <div className="dropdown-content">
           <form>
             <label htmlFor="minPrice">Minimum:</label>
-            <span className="currency">$</span>
             <input type="text" id="minPrice" name="minPrice" value = {minPrice} onChange={(e) => setMinPrice(e.target.value)} />
 
             <label htmlFor="maxPrice">Maximum:</label>
-            <span className="currency">$</span>
             <input type="text" id="maxPrice" name="maxPrice" value = {maxPrice} onChange={(e) => setMaxPrice(e.target.value)} />
 
-            <button type="button" onClick={handleSubmit}>Save Changes</button>
+            <button type="button" onClick={handleSubmit}>Save</button>
           </form>
         </div>
       )}
